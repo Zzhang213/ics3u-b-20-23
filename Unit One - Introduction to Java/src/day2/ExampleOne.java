d317c721c0e622316840c1ebce8f7d7bce16bb69
Abraham Zhang <zzhang@bayviewglen.ca>
2022-09-13 10:40:43 -0400

    Initial Commit

---
+++ Unit One - Introduction to Java/src/day2/ExampleOne.java
@@ -0,0 +1,23 @@
+package day2;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+    public static void main(String[] args) {
+        // This is a comment - it is a message for developers / programmers
+        /*
+         * This is also a comment - it starts with /*
+         *  and ends with star slash
+         */
+
+         System.out.println("Hello");
+         System.out.println("Hello2");      // once println prints it hits enter and goes to the next line
+
+         System.out.print("A");
+         System.out.print("B");     // once print prints it does NOT hits enter and stays on the line
+         System.out.print("C");
+
+    }
+    
+}
---
+++ Unit One - Introduction to Java/src/day2/ExampleTwo.java
@@ -0,0 +1,19 @@
+package day2;
+
+/**
+ * ExampleTwo
+ */
+public class ExampleTwo {
+    public static void main(String[] args) {
+        // escape sequences
+
+        System.out.println("This is \"very\" important!");
+
+        System.out.println("Escape the backslash... \\");   // \ and " have a special purpise and must be escaped so we do \\ and \"
+
+        System.out.println("This is a way\nthat\nI can hit enter on a single line of code.");
+
+
+    }
+    
+}
