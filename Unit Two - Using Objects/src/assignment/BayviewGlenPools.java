1a3aefd77dbd4efbd711f50a4ece42e83978300b
Abraham Zhang <zzhang@bayviewglen.ca>
2022-10-17 11:32:18 -0400

    Assignment v1.2

--- Unit Two - Using Objects/src/assignment/BayviewGlenPools.java
+++ Unit Two - Using Objects/src/assignment/BayviewGlenPools.java
@@ -2,30 +2,28 @@ package assignment;
 
 import java.util.Scanner;
 
-/**
- * BayviewGlenPools
- */
+
 public class BayviewGlenPools {
     public static void main(String[] args) {
 
         Scanner value = new Scanner(System.in);
 
-        System.out.println("What is the length of the pool in foot?");
+        System.out.println("What is the length of the pool?");
         double length = value.nextDouble();  
 
-        System.out.println("What is the width of the pool in foot?");
+        System.out.println("What is the width of the pool?");
         double width = value.nextDouble();  
 
-        System.out.println("What is the depth of the deep end of the pool in foot?");
+        System.out.println("What is the depth of the deep end of the pool?");
         double depthDeepEnd = value.nextDouble();  
 
-        System.out.println("What is the depth of the shallow end of the pool in meter in foot? ");
+        System.out.println("What is the depth of the shallow end of the pool? ");
         double depthShallowEnd = value.nextDouble();    
 
-        System.out.println("What is the length of the shallow end of the pool in meter in foot?");
+        System.out.println("What is the length of the shallow end of the pool?");
         double lengthShallowEnd = value.nextDouble();  
 
-        System.out.println("What is the length of the transition of the pool in meter in foot?");
+        System.out.println("What is the length of the transition of the pool?");
         double transition = value.nextDouble();   
 
         System.out.println("What is the price of the liner per meter square used for the pool?");
@@ -37,16 +35,13 @@ public class BayviewGlenPools {
 
 
         double waterNeeded = 
-        (( lengthDeepEnd * width * depthDeepEnd) + 
+        Math.round(((( lengthDeepEnd * width * depthDeepEnd) + 
         (lengthTransition * differenceDS/ 2 * width) + 
         (depthShallowEnd * (lengthShallowEnd +  lengthTransition) * width)
-        ) * 0.9;
-
-        double gallons = (waterNeeded * 7.481); 
+        ) * 0.9) * 100) /100.0;
 
         double areaLiner = 
-
-        (depthDeepEnd * lengthDeepEnd * 2) + 
+        Math.round(((depthDeepEnd * lengthDeepEnd * 2) + 
         (lengthTransition * differenceDS) +
         ((lengthTransition + lengthShallowEnd) * depthShallowEnd * 2) +
 
@@ -54,13 +49,13 @@ public class BayviewGlenPools {
         (depthShallowEnd * width) +
         (transition * width) +
         (lengthShallowEnd * width) +
-        (lengthDeepEnd * width);
+        (lengthDeepEnd * width)) * 100) /100.0 ;
 
-        double cost = (areaLiner/10.764 * price);
+        double cost = Math.round(areaLiner * price * 100) /100.0;
 
-        System.out.println("The amount of water needed is " + gallons + " gallons");
-        System.out.println("The amount of liner needed to creat the pool is " + areaLiner);
-        System.out.println("The cost of the liner is " + cost); 
+        System.out.println("The amount of water needed is " + waterNeeded); 
+        System.out.println("The amount of liner needed is " + areaLiner);
+        System.out.println("The cost of the liner is " +  "$" + cost); 
 
     }
     
